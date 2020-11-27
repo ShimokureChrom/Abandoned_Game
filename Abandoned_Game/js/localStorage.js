a744b7b8dc5c9c44c461a21bf98c88dcefcd667d
S_D_Chrom <v.hero.aid@gmail.com>
2020-11-27 10:05:42 +0900

    削除機能追加

--- Abandoned_Game/js/localStorage.js
+++ Abandoned_Game/js/localStorage.js
@@ -1,25 +1,21 @@
 // 読込
-function load(data_name) 
-{
-  if(localStorage.getItem(data_name)) 
-    return JSON.parse(localStorage.getItem(data_name));   
-  return [];  
+function load(data_name) {
+  if (localStorage.getItem(data_name))
+    return JSON.parse(localStorage.getItem(data_name));
+  return [];
 }
 
-function load_datas()
-{
+function load_datas() {
   let requests = load('requests');
   let conjureres = load('conjureres');
   let out_data = "";
-  for(let i = 0; i < requests.length; i++)
-  {
+  for (let i = 0; i < requests.length; i++) {
     out_data += requests[i];
     out_data += "<br>";
   }
   document.getElementById("requests_out").innerHTML = out_data;
   out_data = "";
-  for(let i = 0; i < conjureres.length; i++)
-  {
+  for (let i = 0; i < conjureres.length; i++) {
     out_data += conjureres[i];
     out_data += "<br>";
   }
@@ -27,21 +23,26 @@ function load_datas()
 }
 
 // 保存
-function save_request() 
-{
+function save_request() {
   let requests = load('requests');
   let add = document.getElementById("request_name").value;
-  if(add)
+  if (add)
     requests.push(add);
   localStorage.setItem('requests', JSON.stringify(requests));
   load_datas();
 }
-function save_conjurer() 
-{
+function save_conjurer() {
   let conjureres = load('conjureres');
   let add = document.getElementById("conjurer_name").value;
-  if(add)
+  if (add)
     conjureres.push(add);
   localStorage.setItem('conjureres', JSON.stringify(conjureres));
+  load_datas();
+}
+
+//削除
+function delete_datas() {
+  localStorage.setItem('requests', "");
+  localStorage.setItem('conjureres', "");
   load_datas();
 }
