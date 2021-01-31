1251ffa208ccfa79b62687991572888bac0ccc00
ShimokureChrom <v.hero.aid@gmail.com>
2021-01-31 20:18:57 +0900

    データベースとの接続テスト

---
+++ Abandoned_Game/php/dbselect.php
@@ -0,0 +1,28 @@
+<?php
+
+/*$mysqli = new mysqli('localhost', 'root', '246911', 'Abandoned_Game');
+
+if($mysqli->connect_error)
+{
+    echo $mysqli->connect_error;
+    exit();
+}
+else
+{
+    echo 'success!';
+}
+
+$sql2 = "select * from items;";
+$stmt = $mysqli->query($sql2);
+
+while($array=$stmt->fetch_array(MYSQLI_ASSOC))
+{
+    $result=$array['name'];
+}
+
+$mysqli->close;*/
+
+// print($result);
+print('aaa');
+
+?>
