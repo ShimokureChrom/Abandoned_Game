b2014cc977f45b519908e1defca4b7deb3b7dc28
ShimokureChorom <v.hero.aid@gmail.com>
2021-02-03 18:28:50 +0900

    ログイン確認は完成

---
+++ Abandoned_Game/trade_system/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="js/vue.js"></script>
+  <title>大根取引市場</title>
+</head>
+<body>
--- Abandoned_Game/trade_system/index.php
+++ Abandoned_Game/trade_system/index.php
@@ -13,28 +13,22 @@ if (isset($_POST['login']))
   $error_message = "＊ID、もしくはパスワードが間違っています。<br>もう一度入力してください。";
   }
 }
+include("header.php");
+if($error_message){
+  echo $error_message;
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="js/vue.js"></script>
-  <title>大根取引市場</title>
-  <?php
-        if($error_message){
-          echo $error_message;
-        }
-  ?>
-</head>
 <body>
   <h1>メインページ</h1>
-
   <form action="index.php" method="POST">
     <p>ログインID：<input type="text" name="user_name"></p>
     <p>パスワード：<input type="text" name="password"></p>
     <input type="submit" name="login" value="ログイン">
+  </form><br>
+  <form action="register.php" method="POST">
+    <input type="submit" name="register" value="新規登録はこちらから">
   </form>
-</body>
-</html>
+
+<?php
+  include("footer.php");
+?>
